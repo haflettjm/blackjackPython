329cbf4c305db507deed57978b28e79084ad25d8
Jacob Haflett <jacob@datacrunchcorp.com>
2022-09-09 13:45:55 -0400

    initial commit added initial money pot size

---
+++ main.py
@@ -0,0 +1,20 @@
+'''
+    Small Python Project to create ASCII BlackJack game from scratch Idea inspired by big book of small python projects
+'''
+
+
+
+# define scoring
+
+
+
+#Create Boilerplate for main function
+def main():
+    # Define current Money
+    money = 5000
+    print(f"Starting Money: ", money)
+
+
+
+if __name__ == '__main__':
+    main()
